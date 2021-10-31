0ef31d3fdaff5f25baf7338994182a71549d8fc4
Tugdual Grall <tgrall@github.com>
2021-10-31 06:04:23 +0100

    add check files func

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fs = require('fs');
+
+
+async function checkFileExists(filePath) {
+    return fs.promises.access(filePath)
+    .then(() => {
+        core.info(`File ${filePath} exists`);
+        return true;
+    })
+    .catch(() => {
+        core.setFailed(`File ${filePath} is mandatory`);
+        return false;
+    });
+}
 
 (
     async () => {
         try {
-            core.notice("Calling our action");
+            checkFileExists("LICENSE");
+            checkFileExists("README.md");
+
         } catch (error) {
             core.setFailed(error.message);
         }
