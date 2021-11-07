f8e81091c0087435e33b5817e08c5cb1be7c1692
Tugdual Grall <tugdual@gmail.com>
2021-11-07 15:26:53 +0000

    new function with check

--- index.js
+++ index.js
@@ -39,7 +39,43 @@ async function checkFileStartsWithHeader(filePath) {
             checkFileExists("LICENSE");
             checkFileExists("README.md");
 
-            checkFileStartsWithHeader("README.md");
+            if (
+                ! await checkFileStartsWithHeader("README.md")
+            ) {
+                // get token for octokit
+                const token = core.getInput('repo-token');
+                const octokit = new github.getOctokit(token);
+
+                // call octokit to create a check with annotation and details
+                const check = await octokit.rest.checks.create({
+                    owner: github.context.repo.owner,
+                    repo: github.context.repo.repo,
+                    name: 'Readme Validator',
+                    head_sha: github.context.sha,
+                    status: 'completed',
+                    conclusion: 'failure',
+                    output: {
+                        title: 'README.md must start with a title',
+                        summary: 'Please use markdown syntax to create a title',
+                        annotations: [
+                            {
+
+                                path: 'README.md',
+                                start_line: 1,
+                                end_line: 1,
+                                annotation_level: 'failure',
+                                message: 'README.md must start with a header',
+                                start_column: 1,
+                                end_column: 1
+                            }
+                        ]
+                    }
+                });
+
+
+
+            }
+            
 
         } catch (error) {
             core.setFailed(error.message);
